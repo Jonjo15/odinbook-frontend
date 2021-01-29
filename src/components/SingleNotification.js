a607fc76fdfb6973760b4c633891bce78af32911
IVAN <ivanbatur5g@gmail.com>
2021-01-29 21:25:50 +0100

    added some markup to singlenotification.js, need to add populate to notification route for comments

--- src/components/SingleNotification.js
+++ src/components/SingleNotification.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import {Feed} from "semantic-ui-react"
+import {Link} from "react-router-dom"
 export default function SingleNotification({notification}) {
+    let likeContent;
+    let route = notification.type === "accept" ? 
+    ("/users/" + notification.sender._id) : ("/posts/"+ notification.postId)
+    if (notification.postId) {
+        likeContent = "post"
+    }
+    else {
+        likeContent = "comment"
+    }
+    const content = notification.type === "comment"
+     ?
+      (notification.sender.first_name + " commented on your post") 
+      : 
+      notification.type ==="accept" ? 
+      (notification.sender.first_name + " accepted your request") : (notification.sender.first_name+" liked your " +likeContent)
     return (
         <Feed>
-            <Feed.Event
+            <Feed.Event as={Link} to={route}
             //TODO: FINISH
             //TODO: UPDATE NOTIFICAITON ROUTE TO POPULATE CREATOR
-            content={notification.type === "comment"}
+            content={content}
             />
         </Feed>
     )
