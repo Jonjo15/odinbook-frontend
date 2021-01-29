467c5befea1635416dd17a3b48ff5ff28f07921c
IVAN <ivanbatur5g@gmail.com>
2021-01-29 10:50:41 +0100

    Started the profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useAuth } from '../context/authContext'
 
 export default function Profile() {
+    const {state: {currentUser}} = useAuth()
     return (
         <div className="container">
-            <h1>Profile</h1>
+            <h1>Your Profile</h1>
+            <h3>Full Name: {currentUser.first_name} {currentUser.family_name}</h3>
+            <div className="bio">
+                {/* TODO: */}
+            </div>
         </div>
     )
 }
