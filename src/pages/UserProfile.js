6f7d8ce851c7d2895bcd8a02cea061e8ebecfe7b
IVAN <ivanbatur5g@gmail.com>
2021-02-02 15:44:43 +0100

    Added UserProfile Markup, need to finish it

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from "axios"
+import {useParams} from "react-router-dom"
+import { useAuth } from '../context/authContext'
+import {Card} from "semantic-ui-react"
+import Post from "../components/Post"
 
 export default function UserProfile() {
+    const params = useParams()
+    const [user, setUser] = useState(null)
+    const [posts, setPosts] = useState([])
+    const [error, setError] = useState("")
+    const {state: {token, currentUser}} = useAuth()
+    axios.defaults.headers.common["Authorization"] = token
+
+    useEffect(() => {
+        axios.get("http://localhost:5000/users/" + params.userId).then(res => {
+            console.log(res.data)
+            setUser(res.data.user)
+            setPosts(res.data.posts)
+        }).catch(e => {
+            console.log(e.message)
+            setError("Not allowed to see this users page")
+        })
+    }, [params.userId])
     return (
-        <div>
-            <h1>Individual user page</h1>
-        </div>
+        error ? (<p>{error}</p>) : (<div className="grid">
+            <Card
+            // TODO: FIX THIS BUG
+            image={user.profile_pic_url ? user.profile_pic_url : null}
+            header={user.first_name + " " + user.family_name}
+            meta={user.email}
+            description={user.bio ? user.bio : ""}
+            />
+            {posts.map(p => <Post key={p._id} post={p} setPosts={setPosts}/>)}
+        </div>)
+        
     )
 }
